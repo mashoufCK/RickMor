9b8904c4cfc93b0e467e3f5833b34adfc699e8ff
MC <mashuf.ir@gmail.com>
2023-02-05 21:38:50 -0600

    Initial Commit

---
+++ RickMort/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickMort
+//
+//  Created by Mashuf Chowdhury on 2/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
