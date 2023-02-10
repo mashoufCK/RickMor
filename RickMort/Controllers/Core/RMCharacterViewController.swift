06786edf1e63e593ae0eb488b67aa0c2b540dcf1
MC <mashuf.ir@gmail.com>
2023-02-09 20:21:57 -0600

    Setup Character Views, View models

--- RickMort/Controllers/Core/RMCharacterViewController.swift
+++ RickMort/Controllers/Core/RMCharacterViewController.swift
@@ -10,23 +10,21 @@ import UIKit
 /// Controller to show and search for Characters
 final class RMCharacterViewController: UIViewController {
 
+    private let characterListView = CharacterListView()
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
         title = "Characters"
         
+        view.addSubview(characterListView)
+        NSLayoutConstraint.activate([characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                                     characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+                                     characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+                                     characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                                    ])
          
         
-        RMService.shared.execute(.listCharacterRequests,
-                                 expecting: RMGetAllCharacterResponse.self){
-            result in
-            switch result {
-            case .success(let model):
-                print(String(describing: model))
-            case .failure(let error):
-                print(String(describing: error))
-            }
-        }
+       
     }
 }
