4ddcf16ca1c505846e8627f1bba2141e911ff5de
MC <mashuf.ir@gmail.com>
2023-05-08 12:54:25 -0500

    Location Pagination

--- RickMort/Views/Location/RMLocationView.swift
+++ RickMort/Views/Location/RMLocationView.swift
@@ -119,3 +119,45 @@ extension RMLocationView: UITableViewDataSource {
         return cell
     }
 }
+
+extension RMLocationView: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        guard let viewModel = viewModel,
+              !viewModel.cellViewModels.isEmpty,
+              viewModel.shouldShowLoadMoreIndicator,
+              !viewModel.isLoadingMoreLocations
+        else {
+            return
+        }
+        
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false){
+            [weak self] t in
+            
+            let offset = scrollView.contentOffset.y
+            let totalContentHeight = scrollView.contentSize.height
+            let totalScrollViewFixedHeight = scrollView.frame.size.height
+            
+            //120 from the footer
+            if offset >= (totalContentHeight - totalScrollViewFixedHeight - 120) {
+                //print( "should start fetching more" )
+                DispatchQueue.main.async {
+                    self?.showLoadingIndicator()
+                }
+                viewModel.fetchAdditionalLocations()
+                
+                DispatchQueue.main.asyncAfter(deadline: .now()+3, execute: {
+                    self?.tableView.reloadData()
+                })
+            }
+            t.invalidate()
+        }
+    }
+    
+    private func showLoadingIndicator() {
+        
+        let footer = RMTableLoadingFooterView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: 100))
+        tableView.tableFooterView = footer
+    }
+}
---
+++ RickMort/Views/Location/RMTableLoadingFooterView.swift
@@ -0,0 +1,48 @@
+//
+//  RMTableLoadingFooterView.swift
+//  RickMort
+//
+//  Created by Mashuf Chowdhury on 5/8/23.
+//
+
+import UIKit
+
+final class RMTableLoadingFooterView: UIView {
+
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.hidesWhenStopped = true
+        return spinner
+    }()
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(spinner)
+        spinner.startAnimating()
+        
+        addConstraints()
+        
+        frame 
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func addConstraints() {
+        
+        NSLayoutConstraint.activate( [
+        
+            spinner.widthAnchor.constraint(equalToConstant: 55),
+            spinner.heightAnchor.constraint(equalToConstant: 55),
+            spinner.centerXAnchor.constraint(equalTo:  centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+        
+
+        ])
+    }
+}
